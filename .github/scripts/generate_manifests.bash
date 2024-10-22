89404a4b40f887759223366d4fde1488d8e3c6c8
Tommy Gatti <tommy.gatti@anu.edu.au>
2024-10-22 18:09:15 +1100

    Exclude `.git` subdirectories, fix loop

--- .github/scripts/generate_manifests.bash
+++ .github/scripts/generate_manifests.bash
@@ -2,10 +2,10 @@
 # Script for the initial generation of manifest.yaml files in inputs directories
 
 traverse_dir() {
-  dir_without_subdir=$(find "$1" -type d -links 2 ! -empty)
+  dir_without_subdir=$(find "$1" -type d ! -iwholename '*.git*' -links 2 ! -empty)
+
   for dir in $dir_without_subdir; do
     echo "Working on $dir:"
-    ...
     # Find all the files that we want to turn into manifests
     files=$(find "$dir" -maxdepth 1 -type f -printf '%f ')
     if [ -n "$files" ]; then
@@ -14,7 +14,7 @@ traverse_dir() {
       # shellcheck disable=SC2086
       yamf add -n manifest.yaml $files
     fi
-  fi
+  done
 }
 
 # Start traversal from the directory passed as an argument
