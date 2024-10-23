4760f32ab35b22cc9532f37d03afb8eb8e4e3bff
Tommy Gatti <tommy.gatti@anu.edu.au>
2024-10-23 17:05:29 +1100

    Updated messaging, comments, removed `module unload`

--- .github/scripts/generate_manifests.bash
+++ .github/scripts/generate_manifests.bash
@@ -2,17 +2,20 @@
 # Script for the initial generation of manifest.yaml files in inputs directories
 
 if [[ $# -ne 3 ]]; then
-  echo "Error, arguments required are not given."
+  echo "Error, arguments required are not given. Usage: $(basename "$0") YAMF_MODULE_PATH YAMF_MODULE_NAME START_DIR"
+  echo "Where: YAMF_MODULE_PATH is the path to a module folder, YAMF_MODULE_NAME is the name of a module that contains yamf, and START_DIR is a directory to begin searching for leaf nodes."
   exit 1
 fi
 
 yamf_module_path="${1}"
 yamf_module_name="${2}"
+# Defaults to '.' unless the third argument is given
 start_dir="${3:-.}"
 
 module use "$yamf_module_path"
 module load "$yamf_module_name"
 
+# This command finds all the leaf directories: see https://unix.stackexchange.com/questions/68577/find-directories-that-do-not-contain-subdirectories/203991#203991
 dir_without_subdir=$(find "$start_dir" -type d -links 2 ! -empty)
 
 for dir in $dir_without_subdir; do
@@ -28,4 +31,3 @@ for dir in $dir_without_subdir; do
   fi
 done
 
-module unload "$yamf_module_name"
