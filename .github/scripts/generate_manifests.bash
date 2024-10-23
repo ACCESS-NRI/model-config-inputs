63d38c44f148f54dcf53d72c8f944d3b86a64b47
Tommy Gatti <tommy.gatti@anu.edu.au>
2024-10-23 17:24:53 +1100

    Add generate_manifests.bash script for generation of `yamf` manifests at a large scale (#5)
    * Add generate_manifests.bash script for generation of `yamf` manifests at a large scale
    
    * Added required variables for module loading
    
    * Remove creation of manifests for empty leaf
    
    * cd into directory before running `yamf`
    
    * Corrected variable name
    
    * Update .github/scripts/generate_manifests.bash
    
    Co-authored-by: Davide Marchegiani <davide.marchegiani@gmail.com>
    
    * Exclude `.git` subdirectories, fix loop
    
    * cd back to previous directory
    
    * De-functioned the script, removed the exclusion on `.git` subdirs because we don't have them anymore
    
    * Updated messaging, comments, removed `module unload`
    
    ---------
    
    Co-authored-by: Davide Marchegiani <davide.marchegiani@gmail.com>

---
+++ .github/scripts/generate_manifests.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Script for the initial generation of manifest.yaml files in inputs directories
+
+if [[ $# -ne 3 ]]; then
+  echo "Error, arguments required are not given. Usage: $(basename "$0") YAMF_MODULE_PATH YAMF_MODULE_NAME START_DIR"
+  echo "Where: YAMF_MODULE_PATH is the path to a module folder, YAMF_MODULE_NAME is the name of a module that contains yamf, and START_DIR is a directory to begin searching for leaf nodes."
+  exit 1
+fi
+
+yamf_module_path="${1}"
+yamf_module_name="${2}"
+# Defaults to '.' unless the third argument is given
+start_dir="${3:-.}"
+
+module use "$yamf_module_path"
+module load "$yamf_module_name"
+
+# This command finds all the leaf directories: see https://unix.stackexchange.com/questions/68577/find-directories-that-do-not-contain-subdirectories/203991#203991
+dir_without_subdir=$(find "$start_dir" -type d -links 2 ! -empty)
+
+for dir in $dir_without_subdir; do
+  echo "Working on $dir:"
+  # Find all the files that we want to turn into manifests
+  files=$(find "$dir" -maxdepth 1 -type f -printf '%f ')
+  if [ -n "$files" ]; then
+      # If there are some files, add them using yamf
+      cd "$dir" || exit 2
+      # shellcheck disable=SC2086
+      yamf add -n manifest.yaml $files
+      cd - || exit 2
+  fi
+done
+
