ff326c619db935cf95f98e1061d203c89bcf150a
Tommy Gatti <tommy.gatti@anu.edu.au>
2024-10-23 12:15:06 +1100

    De-functioned the script, removed the exclusion on `.git` subdirs because we don't have them anymore

--- .github/scripts/generate_manifests.bash
+++ .github/scripts/generate_manifests.bash
@@ -1,24 +1,6 @@
 #!/bin/bash
 # Script for the initial generation of manifest.yaml files in inputs directories
 
-traverse_dir() {
-  dir_without_subdir=$(find "$1" -type d ! -iwholename '*.git*' -links 2 ! -empty)
-
-  for dir in $dir_without_subdir; do
-    echo "Working on $dir:"
-    # Find all the files that we want to turn into manifests
-    files=$(find "$dir" -maxdepth 1 -type f -printf '%f ')
-    if [ -n "$files" ]; then
-      # If there are some files, add them using yamf
-      cd "$dir" || exit 2
-      # shellcheck disable=SC2086
-      yamf add -n manifest.yaml $files
-      cd - || exit 2
-    fi
-  done
-}
-
-# Start traversal from the directory passed as an argument
 if [[ $# -ne 3 ]]; then
   echo "Error, arguments required are not given."
   exit 1
@@ -31,6 +13,19 @@ start_dir="${3:-.}"
 module use "$yamf_module_path"
 module load "$yamf_module_name"
 
-traverse_dir "$start_dir"
+dir_without_subdir=$(find "$start_dir" -type d -links 2 ! -empty)
+
+for dir in $dir_without_subdir; do
+  echo "Working on $dir:"
+  # Find all the files that we want to turn into manifests
+  files=$(find "$dir" -maxdepth 1 -type f -printf '%f ')
+  if [ -n "$files" ]; then
+      # If there are some files, add them using yamf
+      cd "$dir" || exit 2
+      # shellcheck disable=SC2086
+      yamf add -n manifest.yaml $files
+      cd - || exit 2
+  fi
+done
 
 module unload "$yamf_module_name"
